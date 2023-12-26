7b5fcfaed129b5d88f97c795d786a4f044998ba3
呂佶 <ji8058882@gmail.com>
2023-12-26 23:58:09 +0800

    第二節：利用套件執行任務(2)

--- 2.R
+++ 2.R
@@ -6,3 +6,20 @@ URL = "https://reg.ntuh.gov.tw/EmgInfoBoard/NTUHEmgInfo.aspx"
 website = read_html(URL)
 needed_txt = website %>% html_nodes("tr") %>% html_text()
 needed_txt
+
+#利用套件執行任務(2)
+#那現在我們來做一個有趣的任務，PTT有一個看板叫做AllTogether，上面有許多單身男女會在上面徵男友或女友。
+#讓我們解析一下這個網頁吧，其中你會注意到我們對「a」這個標籤是有興趣的，因為它包含著文章的連結：
+URL = "https://www.ptt.cc/bbs/AllTogether/index3245.html"
+website = read_html(URL)
+
+needed_html = website %>% html_nodes("a")
+needed_html
+
+needed_txt = needed_html %>% html_text()
+needed_txt
+#我比較有興趣的是徵女文，我們用這種方式能找到徵男文的位置在哪：
+intrested_pos = grep("[徵女]", needed_txt, fixed = TRUE)
+needed_txt[intrested_pos]
+#我更有興趣的可能是這篇文章的連結，你會發現文章連結藏在needed_html裡面，我們可以透過這種方式拿到：
+needed_link = needed_html[intrested_pos] %>% html_attr("href")
