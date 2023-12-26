e302f7821e1334016ff42489cfe8a59317a0082d
呂佶 <ji8058882@gmail.com>
2023-12-27 00:08:31 +0800

    第三節：使用cookie(2)

--- 3.R
+++ 3.R
@@ -4,3 +4,15 @@ URL = 'https://www.ptt.cc/bbs/Gossiping/index.html'
 website = read_html(URL)
 website
 #這個原因是因為直接訪問八卦(Gossiping)版你會看到這個畫面：
+
+#使用cookie(2)
+#我們需要先使用一般的瀏覽器訪問該網站(最好使用無痕視窗)，接著你在選擇滿18歲以後你的電腦將會記錄下這個答案，我們叫他cookie。
+#你可以透過下面這個方式找到電腦目前的cookie共有哪些：
+#接著你會發現原來在我們點擊這個選項之後，電腦會記錄住一個cookie叫做「over18」，而他的值是「1」：
+
+
+
+
+
+
+
