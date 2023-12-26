a2f0ed69a7dc79a5f71139ae36bbe9462fc57a32
呂佶 <ji8058882@gmail.com>
2023-12-27 00:06:35 +0800

    第三節：使用cookie(1)

---
+++ 3.R
@@ -0,0 +1,6 @@
+###第三節：使用cookie(1)
+#如果你用前面的方法訪問八卦(Gossiping)版，你會發現你沒有辦法做：
+URL = 'https://www.ptt.cc/bbs/Gossiping/index.html'
+website = read_html(URL)
+website
+#這個原因是因為直接訪問八卦(Gossiping)版你會看到這個畫面：
